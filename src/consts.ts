feb30c8cfb549aeac633fe63b575c2d9e481d420
Alexandru Bereghici <abereghici@tripadvisor.com>
2025-04-02 16:41:42 +0300

    fix site url

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "bereghici.dev";
+export const SITE_TITLE = "alexandru.bereghici.dev";
 export const SITE_AUTHOR = "Alexandru Bereghici";
 export const SITE_DESCRIPTION =
   "Software engineer based in Chisinau, Republic of Moldova";
-export const SITE_URL = "https://bereghici.dev";
+export const SITE_URL = "https://alexandru.bereghici.dev";
 
 export const EMAIL = "alexandru.brg@gmail.com";
 export const LINKEDIN = "https://md.linkedin.com/in/alexandrubereghici";
